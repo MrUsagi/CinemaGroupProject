4a9e16a811460387e05029647027c15c05601b62
Artem <artemermolaev0@gmail.com>
2020-09-09 19:57:44 +0300

    Itial windows

--- ProjectWindows/MainWindow.xaml.cs
+++ ProjectWindows/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CinemaProject
         {
             InitializeComponent();
         }
+
+        private void MovieClick_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ ProjectWindows/SingupFrom.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElectroShop
+{
+    /// <summary>
+    /// Interaction logic for SingupFrom.xaml
+    /// </summary>
+    public partial class SingupFrom : Window
+    {
+        public SingupFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void SingUp_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Phone_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+        private void Login_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Password_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}

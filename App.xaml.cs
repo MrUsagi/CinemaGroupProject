0a1e9e2b1fb88c0b5ceccec1e17de405208dc430
MrUsagi <anavisikim1@gmail.com>
2020-09-10 14:40:42 +0300

    Configure DI

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using CinemaProject.DataLayer.Context;
+using CinemaProject.DataLayer.Repository;
+using CinemaProject.DataLayer.UOW;
+using CinemaProject.DataLayer.UOW.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +26,14 @@ namespace CinemaProject
         private void ConfigurationService(IServiceCollection services)
         {
             services.AddSingleton<CinemaContext>();
+            services.AddSingleton<FilmRepository>();
+            services.AddSingleton<FilmStoryRepository>();
+            services.AddSingleton<HallRepository>();
+            services.AddSingleton<PlaceRepository>();
+            services.AddSingleton<RowRepository>();
+            services.AddSingleton<ShowRepository>();
+            services.AddSingleton<UserRepository>();
+            services.AddSingleton(typeof(IUnitOfWork), typeof(UnitOfWork));
             services.AddTransient(typeof(MainWindow));
 
             services.AddDbContext<CinemaContext>(option => option.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
